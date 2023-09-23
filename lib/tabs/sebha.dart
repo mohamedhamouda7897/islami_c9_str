196f89077170a05b958a1cb9acf4606ac28e9d5d
Mohamed Hamouda <mohamed.hamouda7897@gmail.com>
2023-09-23 13:47:16 +0300

    Revert "change color to red"
    This reverts commit 797d017222989cfe9dfd1983b74e230d1fed5cb0.

--- lib/tabs/sebha.dart
+++ lib/tabs/sebha.dart
@@ -6,8 +6,7 @@ class SebhaTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.red,
-
+      color: Colors.pinkAccent,
     );
   }
 }
